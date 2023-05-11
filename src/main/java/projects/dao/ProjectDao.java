f28690745ba3e76be436e6c6da75386d21f85b1c
Cheenue Thao <cheenuethao@gmail.com>
2023-05-11 18:42:16 -0500

    Week 10 coding assignment

--- src/main/java/projects/dao/ProjectDao.java
+++ src/main/java/projects/dao/ProjectDao.java
@@ -4,6 +4,7 @@ import projects.entity.Project;
 import projects.exception.DbConnection;
 import projects.exception.DbException;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,18 +17,18 @@ import provided.util.DaoBase;
 public class ProjectDao extends DaoBase {
     private static final String CATEGORY_TABLE = "category";
     private static final String MATERIAL_TABLE = "material";
-    private static final String PROJECT_TABLE = "category";
+    private static final String PROJECT_TABLE = "project";
     private static final String PROJECT_CATEGORY_TABLE = "project_category";
-    private static final String STEP_TABLE = "category";
+    private static final String STEP_TABLE = "step";
 
     public Project insertProject(Project project) {
         //@formatter:off
-        System.out.println(project);
-        String sql = "" + "INSERTS INTO " + PROJECT_TABLE + " "
+
+        String sql = ""
+                + "INSERT INTO " + PROJECT_TABLE + " "
                 + "(project_name, estimated_hours, actual_hours, difficulty, notes) "
                 + "VALUES "
                 + "(?, ?, ?, ?, ?)";
-        System.out.println(sql);
         //@formatter: on
 
         try (Connection conn = DbConnection.getConnection()) {
@@ -35,9 +36,9 @@ public class ProjectDao extends DaoBase {
 
             try (PreparedStatement stmt = conn.prepareStatement(sql)) {
                 setParameter(stmt, 1, project.getProjectName(), String.class);
-                setParameter(stmt, 2, project.getEstimatedHours(), String.class);
-                setParameter(stmt, 3, project.getActualHours(), String.class);
-                setParameter(stmt, 4, project.getDifficulty(), String.class);
+                setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+                setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+                setParameter(stmt, 4, project.getDifficulty(), Integer.class);
                 setParameter(stmt, 5, project.getNotes(), String.class);
 
                 stmt.executeUpdate();
@@ -52,7 +53,7 @@ public class ProjectDao extends DaoBase {
                 throw new RuntimeException(e);
             }
         } catch (SQLException e) {
-            throw new DbException();
+            throw new DbException(e);
         }
     }
 
@@ -67,14 +68,15 @@ public class ProjectDao extends DaoBase {
                     List<Project> projects = new LinkedList<>();
 
                     while(rs.next()) {
-                        Project project = new Project();
 
-                        project.setActualHours(rs.getBigDecimal("actual_hours"));
-                        project.setDifficulty(rs.getObject("difficulty", Integer.class));
-                        project.setEstimatedHours(rs.getBigDecimal("estimated_hours"));
-                        project.setNotes(rs.getString("notes"));
-                        project.setProjectId(rs.getObject("project_id", Integer.class));
-                        project.setProjectName(rs.getString("project_name"));
+//                        Project project = new Project();
+//
+//                        project.setActualHours(rs.getBigDecimal("actual_hours"));
+//                        project.setDifficulty(rs.getObject("difficulty", Integer.class));
+//                        project.setEstimatedHours(rs.getBigDecimal("estimated_hours"));
+//                        project.setNotes(rs.getString("notes"));
+//                        project.setProjectId(rs.getObject("project_id", Integer.class));
+//                        project.setProjectName(rs.getString("project_name"));
 
                     }
                     return projects;
